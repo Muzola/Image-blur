aea881b7f9397c41623adde133d8cd99c1fb04b0
Anthony <amazzolaphoto@gmail.com>
2019-09-10 18:52:00 +0000

    img blur 3

--- image_blur1.rb
+++ image_blur1.rb
@@ -1,7 +1,6 @@
 class Image
-  attr_accessor :picture 
 
-  def initialize(picture)
+  def initialize (picture)
     @picture = picture
   end
 
@@ -18,4 +17,5 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+
 image.output_image
